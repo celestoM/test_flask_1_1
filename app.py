239c4df6d25b77160872a8f6807319110c5fecfc
Renegmz <renegmz21@gmail.com>
2023-08-09 21:15:44 -0400

    [ADD]: Endpoint read

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-rom flask import Flask,request
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route("/init", methods=["GET"])
-def init():
+@app.route("/inicio", methods=["GET"])
+def inicio():
     return {"payload":"welcome to my project3"}
 
-@app.route("/init/:<content>", methods=["GET"])
+@app.route("/read/:<content>", methods=["GET"])
 def read(content):
+    return {"payload":content}
+
+@app.route("/init/:<content>", methods=["GET"])
+def init(content):
     if content=="alfa":
         return {"payload":content}
     else:
